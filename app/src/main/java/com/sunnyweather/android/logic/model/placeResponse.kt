e9acc462d8a5deaf36262125d432c3b4511d6888
LB <1203045873@qq.com>
2020-06-25 17:34:50 +0800

    搜索功能

---
+++ app/src/main/java/com/sunnyweather/android/logic/model/placeResponse.kt
@@ -0,0 +1,10 @@
+package com.sunnyweather.android.logic.model
+
+
+import com.google.gson.annotations.SerializedName
+
+class placeResponse(val status: String, val places: List<Place>)
+
+class Place(val name: String, val location: Location, @SerializedName("formatted_address") val address: String)
+
+class Location(val lng: String, val lat: String)
